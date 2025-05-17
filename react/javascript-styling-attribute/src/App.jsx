36128c71ca649b7e9ce24f5dabd68b1dfba1c76b
ayush109-2528 <ayushrajput109@gmail.com>
2025-05-17 15:48:54 +0530

    css in react

---
+++ react/javascript-styling-attribute/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='heading1' contenteditable = "true" spellCheck="false ">My Favorite Food</h1>
+      <ul>
+        <li>chole Bathure</li>
+        <li>pizza</li>
+        <li>burger</li>
+      </ul>
+      <img src="https://picsum.photos/200/300" alt="" />
+      <img src="https://picsum.photos/200/300" alt="" />
+      <img src="https://picsum.photos/200/300" alt="" />
+    </>
+  )
+}
+
+export default App
