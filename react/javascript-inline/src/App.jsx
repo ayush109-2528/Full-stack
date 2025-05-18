85944d44a13c909a6fea911dee5f73d6bf5a85c5
ayush109-2528 <ayushrajput109@gmail.com>
2025-05-18 13:57:50 +0530

    inline css

---
+++ react/javascript-inline/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const style ={
+    color: "red",
+    fontSize: "20px",
+    border: "3px solid black"
+  }
+  return (
+    <>
+      <p style={style}>Hello World</p>
+    </>
+  )
+}
+
+export default App
