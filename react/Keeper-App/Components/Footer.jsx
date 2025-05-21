5f6904759a04cde17ad3e55fd2440fb65437b640
ayush109-2528 <ayushrajput109@gmail.com>
2025-05-21 14:43:46 +0530

    keeper App part 1

---
+++ react/Keeper-App/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import Reacr from "react";
+
+function Footer() {
+  return (
+    <>
+      <footer>
+        <p>Copyright {new Date().getFullYear()}</p>
+      </footer>
+    </>
+  );
+}
+export default Footer;
---
+++ react/Keeper-App/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Header from '../Components/Header'
+import Footer from '../Components/Footer'
+import Note from '../Components/Note'
+function App() {
+ 
+
+  return (
+    <>
+    <Header></Header>
+    <Note></Note>
+    <Footer></Footer>
+    </>
+  )
+}
+
+export default App
