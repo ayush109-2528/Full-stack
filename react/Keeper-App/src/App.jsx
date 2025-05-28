9f4121d179df5ead45b32f5907fd8ffd0573e219
ayush109-2528 <ayushrajput109@gmail.com>
2025-05-28 16:16:17 +0530

    keeper app add dynamic notes nd arrow function

--- react/Keeper-App/src/App.jsx
+++ react/Keeper-App/src/App.jsx
@@ -1,18 +1,22 @@
-import { useState } from 'react'
-import './App.css'
-import Header from '../Components/Header'
-import Footer from '../Components/Footer'
-import Note from '../Components/Note'
-function App() {
- 
+import { useState } from "react";
+import "./App.css";
+import Header from "../Components/Header";
+import Footer from "../Components/Footer";
+import Note from "../Components/Note";
+import notes from "../Components/notes";
 
+function App() {
   return (
     <>
-    <Header />
-    <Note />
-    <Footer />
+      <Header />
+      <div>
+        {notes.map((note) => (
+          <Note key={note.key} title={note.title} content={note.content} />
+        ))};
+      </div>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
