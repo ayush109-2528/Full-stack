74a7adf555b6dc0709d9aff7fcc99d14c2d4abdb
ayush109-2528 <ayushrajput109@gmail.com>
2025-06-08 16:05:46 +0530

    class components vs hook components

---
+++ react/React-Class/src/App.jsx
@@ -0,0 +1,14 @@
+import React,{ useState } from 'react'
+import './App.css'
+import ClassComponent from './assets/ClassComponents'
+
+class App extends React.Component {
+
+  render() {
+    return <>
+    <ClassComponent />
+    </>
+  }
+}
+
+export default App
