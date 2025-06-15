a2cabf255af54291e49e4429a273c19066ac809e
ayush109-2528 <ayushrajput109@gmail.com>
2025-06-15 16:38:27 +0530

    component managing practice

---
+++ react/manageing-components-practice/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import "./App.css";
+import ToDoItem from "./assets/todoItem";
+import InputArea from "./assets/inputArea";
+
+function App() {
+  // const [inputText, setInputText] = useState("");
+  const [items, setItems] = useState([]);
+
+  function addItem(inputText) {
+    setItems((prevItems) => {
+      return [...prevItems, inputText];
+    });
+  }
+
+  function deleteItem(id) {
+    setItems((prevItems) => {
+      return prevItems.filter((item, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
+    <div className="container">
+      <div className="heading">
+        <h1>To-Do List</h1>
+      </div>
+      <InputArea 
+      onAdd={addItem}
+      />
+      <div>
+        <ul>
+          {items.map((todoItem, index) => (
+            <ToDoItem
+              key={index}
+              id={index}
+              text={todoItem}
+              onChecked={deleteItem}
+            />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
